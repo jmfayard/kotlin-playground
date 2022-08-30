cb2c8f5a67ff8b3953af5a4bd2ca0c136e21eddf
Jean-Michel Fayard <459464+jmfayard@users.noreply.github.com>
2022-08-30 09:34:36 +0200

    kotlin: table driven testing

--- src/test/kotlin/kotest/TableTesting.kt
+++ src/test/kotlin/kotest/TableTesting.kt
@@ -1,14 +1,25 @@
 package kotest
 
+import io.kotest.assertions.asClue
+import io.kotest.data.Headers3
+import io.kotest.data.Row3
 import io.kotest.data.Table3
 import io.kotest.data.forAll
 import io.kotest.data.headers
 import io.kotest.data.row
 import io.kotest.data.table
 import io.kotest.inspectors.forAll
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.shouldContainAll
+import io.kotest.matchers.collections.shouldNotBeEmpty
+import io.kotest.matchers.file.shouldExist
+import io.kotest.matchers.ints.shouldBeGreaterThan
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.Disabled
+import io.kotest.matchers.shouldNotBe
 import org.junit.jupiter.api.Test
+import java.io.File
+
+val testResources: File = File("src/test/resources").apply(File::shouldExist)
 
 class TableTesting {
 
@@ -37,10 +48,74 @@ class TableTesting {
         }
     }
 
-    @Test
-    @Disabled
-    fun readingFromCsv() {
-        // see ticket https://github.com/kotest/kotest/issues/3179
-    }
+    val header = "id, login, location, bio"
 
+    @Test
+    fun `table pojo`() {
+        val table = table(
+            headers("id", "login", "bio"),
+            row(4, "Jean-Michel Fayard", Bio.Jmfayard),
+            row(6, "Louis CAD", Bio.LouisCAD)
+        )
+
+        // forAll
+        table.forAll { id, login, bio ->
+            id shouldBeGreaterThan 0
+            login shouldNotBe ""
+        }
+
+        // write to CSV
+        val tableFile = testResources.resolve("kotest/write.table")
+        table.writeTo(tableFile)
+
+        // read from CSV
+        val tableFromResources = table(
+            source = tableFile,
+            headers = headers("id", "login", "bio"),
+            transform = { a: String, b: String, c: String ->
+                row(a.toInt(), b, Bio.valueOf(c))
+            }
+        )
+        println(tableFromResources)
+        tableFromResources shouldBe table
+    }
+}
+
+enum class Bio {
+    Jmfayard, LouisCAD
+}
+
+private const val separator = "|"
+
+fun <A, B, C> Table3<A, B, C>.writeTo(destination: File) {
+    destination.extension shouldBe "table"
+    val rowsContent = rows.joinToString("\n") { it.values().joinToString(separator) }
+    val header = headers.values().joinToString(separator)
+    destination.writeText(header + "\n" + rowsContent)
+}
+
+fun <A, B, C> table(
+    source: File,
+    headers: Headers3,
+    transform: (String, String, String) -> Row3<A, B, C>
+): Table3<A, B, C> {
+    source.shouldExist()
+    source.extension shouldBe "table"
+    val lines = source.readText().lines()
+    lines.shouldNotBeEmpty()
+    val header = lines.first().replace(" ", "").split("|")
+    headers.values() shouldContainAll header
+    val rows = lines.drop(1)
+        .filter { it.contains("|") }
+        .map { line -> line.split("|").map(String::trim) }
+    val size = headers.values().size
+
+    "rows should have the same size as the header".asClue {
+        rows.filter { it.size < size }.take(3).shouldBeEmpty()
+    }
+    val transformedRows = rows.map { list ->
+        val (a, b, c) = list
+        transform(a, b, c)
+    }
+    return table(headers, *transformedRows.toTypedArray())
 }
